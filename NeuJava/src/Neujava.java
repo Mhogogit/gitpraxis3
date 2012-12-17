13f8cee30dd4ae9fc7f5e1fe8147a084e2f3eec7
Fayad <fayadconfuego@hotmail.com>
2012-12-17 19:02:57 +0100

    Neu

--- NeuJava/src/Neujava.java
+++ NeuJava/src/Neujava.java
@@ -1,7 +1,7 @@
 
 public class Neujava {
 	public static void main(String[] args) {
-	       System.out.println("Salut Monsieur Fayad1!");
+	       System.out.println("Salut Monsieur Fayad NEU!");
 	        
 	    }
 	      
